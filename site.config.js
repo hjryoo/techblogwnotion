f4bdbde8de3d9a43ef23cab9e4e953325a47522a
HyeJin Ryoo <56506753+hjryoo@users.noreply.github.com>
2026-01-01 20:35:01 +0900

    Update site.config.js
    update it with personal settings

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "HyeJinRyoo",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "developer",
+    bio: "I'm a developer",
+    email: "hjryoo31@gmail.com",
+    linkedin: "hjryoo",
+    github: "hjryoo",
     instagram: "",
   },
   projects: [
@@ -18,9 +18,9 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "MyTechBlog",
+    description: "Welcome to my personal techblog",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
@@ -31,7 +31,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
+    pageId: "2db414ae829a81f0b58c000c4b31e8f6",
   },
 
   // plugin configuration (optional)
